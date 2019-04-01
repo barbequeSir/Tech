26bfecfe3f056ebeec4c750ba68b61b2da645174
qinbobo <qinbobo@elex-tech.com>
2019-04-01 19:02:47 +0800

    1

---
+++ AssetBundle/AssetBundle/Assets/Scripts/test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class test : MonoBehaviour {    
+    AssetBundleManifest abm;
+    // Use this for initialization
+
+    void Start() {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText() {
+        UnityWebRequest www = UnityWebRequest.Get("file:///D:/SVN/Tech/AssetBundle/Http/WWWRoot/Window/BundleAssets/versionfile.txt");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            byte[] results = www.downloadHandler.data;
+
+            ///AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+        }
+    }   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
